5f8ecb9c63c806eb0b9db66483a44e664b194832
fc56943 <fc56943@alunos.ciencias.ulisboa.pt>
2024-09-19 17:03:07 +0100

    adding a new activity

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstapp.ui.theme.FirstAppTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +53,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Cyan) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24 .dp)
         )
     }
 }
